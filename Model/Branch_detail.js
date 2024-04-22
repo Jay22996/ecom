08d78cc97575161a83c51430b12dc91de059ec39
Jay22996 <jay00289@gmail.com>
2024-04-22 13:32:02 +0530

    d

--- Model/Branch_detail.js
+++ Model/Branch_detail.js
@@ -6,7 +6,13 @@ var branch_schema = new mongoose.Schema({
     mobile_num:{type:String},
     lat: {type:String},
     log: {type:String},
-    address:{type:String}
+    email:{type:String},
+    password:{type:String},
+    address:{type:String},
+    pincode:{type:Number},
+    city:{type:String},
+    state:{type:String},
+    country:{type:String},
 })
 
 module.exports = mongoose. model('branch_detail',branch_schema);
