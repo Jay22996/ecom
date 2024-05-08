788cbb08294b5f19f28398cccbb644c132fa9197
Jay22996 <jay00289@gmail.com>
2024-05-08 11:09:25 +0530

    s

--- Model/Ads_details.js
+++ Model/Ads_details.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 
 var Abs_schema = new mongoose.Schema({
     title : {type:String},
-    active_status : {type:String},
+    active_status : {type:String,default:"active"},
     imd_url:{type:String},
-    date: {type:String}
+    date: {type:String,default:Date.now()}
 })
 
 module.exports = mongoose. model('Abs_detail',Abs_schema);
