1459cbb183fdcee7fed9614d0e9791d78833e41b
Jay22996 <jay00289@gmail.com>
2024-05-08 12:07:03 +0530

    ss

--- Controller/Abs_controller.js
+++ Controller/Abs_controller.js
@@ -31,17 +31,20 @@ exports.showabs = async (req,res)=>{
 exports.updateabs = async (req,res)=>{
     var id = req.params.id
     var data = await abs.findByIdAndUpdate(id,req.body)
+    var data1 = await abs.find()
+
     res.status(200).json({
         status:"show abs",
-        data
+        data1
     })
 }
 
 exports.deleteabs = async (req,res)=>{
     var id = req.params.id
     var data = await abs.findByIdAndDelete(id)
+    var data1 = await abs.find()
     res.status(200).json({
         status:"show abs",
-        data 
+        data1
     })
 }
