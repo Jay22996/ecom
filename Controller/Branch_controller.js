3170ca0f97dab7966a866fef8a1c233d44a394c7
Jay22996 <jay00289@gmail.com>
2024-05-21 12:25:05 +0530

    sadf

--- Controller/Branch_controller.js
+++ Controller/Branch_controller.js
@@ -53,7 +53,7 @@ exports.updatebranch = async (req, res) => {
   var data = await branch.findByIdAndUpdate(id, req.body);
   
   res.status(200).json({
-    status: "delete branch",
+    status: "update branch",
     data,
   });
 };
