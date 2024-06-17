0f8cd5457e3e69f09d3b02719958af840e2f419f
Jay22996 <jay00289@gmail.com>
2024-06-17 15:34:23 +0530

    first commit

--- app.js
+++ app.js
@@ -19,9 +19,7 @@ var orderRouter = require("./routes/Order");
 var paymentRouter = require("./routes/Payment");
 
 const corsOptions = {
-  origin:"https://pc-master-admin-web-ce7r.vercel.app", 
-  // origin: "http://localhost:3000",
-
+  origin: "http://localhost:3000",
   methods: "GET , POST",
   credentials: true,
   
